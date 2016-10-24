80055ec2c71a8b31cd0d00be7bc7ce952be38b08
Mao <ml1480@york.ac.uk>
2016-10-24 23:56:30 +0100

    implement reinforcementLearning function.  python can call it to do RL

--- GAS/src/com/gas/TestMain.java
+++ GAS/src/com/gas/TestMain.java
@@ -17,6 +17,7 @@ public class TestMain {
 
     private String reinforcementLearning(String rewardVector){
         Map<String,Double> para = new HashMap<String,Double>();
+        //需要一个  字符串——>map  个解析器 java Python两边实现，通过字符串传参数。
         para.put("x",  0.1);
         para.put("x2", 0.1);
         para.put("x3", 0.1);
@@ -51,14 +52,6 @@ public class TestMain {
     }
 
     public String expert (){
-        return "";
-    }
-    public static void main(String[] args){
-        TestMain test = new TestMain();
-        test.rl("");
-
- /*
-
         RewardFunc reward = new GridRewardFunc();
         Logger log = new GridLogger("GridLogger");
         Environment env = new GridEnvironment();
@@ -73,20 +66,12 @@ public class TestMain {
         expertAgent.initialize();
         List<List<State>> runHistory = expertAgent.runTrajectory(300);
         GridAnalyser analyser = new GridAnalyser(0.8);
-        Vector rv = analyser.preprocessing(runHistory);
-        rv.print();
-
-
-        GridValue value = new GridValue();
-        double discount = 0.8;
-        double learn_rate = 0.1;
-        double epsilon = 0.8;
-        GridRLAgent agent_rl = new GridRLAgent(env, reward, log, actions, value, discount,learn_rate,epsilon);
-        agent_rl.initialize();
-        agent_rl.runTrajectory(1000000);
-
-        value.printQV();
-
-  */
+        StatePolyVector rv = analyser.preprocessing(runHistory);
+        //rv.print();
+        return rv.toString();
+    }
+    public static void main(String[] args){
+        TestMain test = new TestMain();
+        test.reinforcementLearning("");
     }
 }
